77853dd4d5a34dc7a065ac133fc2d75442bdf5ef
Daniel-29 <daniel.alex.barrera@hotmail.com>
2021-11-17 13:10:00 -0600

    clear repo

--- frontendAG/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:'https://192.168.1.37:3060/',
-  TextValidation:'[a-zA-Z0-9 ]*',
-  PasswordValidation:'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}',
-  LettersValidation:'[a-zA-Z ]*',
-  NumbersValidation:'[0-9]*',
-};
